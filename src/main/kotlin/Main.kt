02b9283b6f80d24734f3738b37290e3750c96557
Zaid Khan <zaiddkhhan@gmail.com>
2024-06-09 12:00:34 +0530

    maxLengthBetweenEqualCharacters solved

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,10 @@
 import arrays_easy.*
-import arrays_medium.*
-import codewars.chooseBestSum
-import hackerrank.*
-import memoizations.canSum
-import memoizations.getFib
-import memoizations.gridTraveller
-import java.util.Calendar
 
 
 fun main(args : Array<String>){
 
 
-    println(makeEqual(arrayOf("caaaaa","aaaaaaaaa","a","bbb","bbbbbbbbb","bbb","cc","cccccccccccc","ccccccc","ccccccc","cc","cccc","c","cccccccc","c")))
-    println(makeEqual(arrayOf("bc","de")))
-    println(makeEqual(arrayOf("abc","aabc","bc")))
-    println(makeEqual(arrayOf("ab","a")))
+    println(maxLengthBetweenEqualCharacters("ojdncpvyneq"))
 }
 
 
