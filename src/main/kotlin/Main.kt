4ffa6eadf4196f98248fc30c416e9f8ba749187a
Zaid Khan <zaiddkhhan@gmail.com>
2024-06-18 21:16:43 +0530

    check possibility done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,20 +10,8 @@ import java.util.Calendar
 
 fun main(args : Array<String>){
 
-    var obj = NumMatrix(arrayOf(
-            intArrayOf(3, 0, 1, 4, 2),
-            intArrayOf(5, 6, 3, 2, 1),
-            intArrayOf(1, 2, 0, 1, 5),
-            intArrayOf(4, 1, 0, 1, 7),
-            intArrayOf(1, 0, 3, 0, 5)
-    ))
-    var param_1 = obj.sumRegion(2, 1, 4, 3)
-    var param_2 = obj.sumRegion(1, 1, 2, 2)
-    var param_3 = obj.sumRegion(1, 2, 2, 4)
 
-    println(param_1)
-    println(param_2)
-    println(param_3)
+    println(checkPossibility(intArrayOf(-1,4,2,3    )))
 
 }
 
