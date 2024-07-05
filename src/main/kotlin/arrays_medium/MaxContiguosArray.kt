45634a5d6ce65162af3ebf740dfb7c78d82aa304
Zaid Khan <zaiddkhhan@gmail.com>
2024-07-05 22:40:26 +0530

    max contiguos array done

---
+++ src/main/kotlin/arrays_medium/MaxContiguosArray.kt
@@ -0,0 +1,33 @@
+package arrays_medium
+
+import java.util.Stack
+import kotlin.math.abs
+
+fun findMaxLength(nums: IntArray): Int {
+
+    var numZero = 0
+    var numOne = 0
+    var max = 0
+    val mapp = mutableMapOf<Int,Int>()
+
+    nums.forEachIndexed { index, i ->
+        if(i == 0){
+            numZero++
+        }else{
+            numOne++
+        }
+        if(mapp[numOne - numZero] == null){
+            mapp[numOne - numZero] = i
+        }
+        if(numZero == numOne){
+            max = numZero + numOne
+        }else{
+            val index = mapp[numOne-numZero]!!
+            max = maxOf(max,i-index)
+        }
+
+    }
+
+    return max
+
+}
