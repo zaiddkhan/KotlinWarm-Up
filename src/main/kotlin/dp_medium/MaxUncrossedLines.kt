a815a34ac6dca190b9faf85ab1a90f8f8d1587c3
Zaid Khan <zaiddkhhan@gmail.com>
2024-11-30 01:03:55 +0530

    dp solution done

--- src/main/kotlin/dp_medium/MaxUncrossedLines.kt
+++ src/main/kotlin/dp_medium/MaxUncrossedLines.kt
@@ -2,21 +2,19 @@ package dp_medium
 
 fun maxUncrossedLines(nums1: IntArray, nums2: IntArray): Int {
 
-    val cache = mutableMapOf<Pair<Int,Int>, Int>()
-
-    fun dfs(i :Int,j : Int) : Int{
-        if(i == nums1.size || j == nums2.size){
-            return 0
+    val dp = MutableList(nums2.size + 1) { MutableList(nums1.size + 1) { 0 } }
+    for (i in 0 until nums2.size) { // Loop over nums2 indices
+        for (j in 0 until nums1.size) { // Loop over nums1 indices
+            if (nums2[i] == nums1[j]) {
+                dp[i + 1][j + 1] = 1 + dp[i][j]
+            } else {
+                dp[i + 1][j + 1] = maxOf(
+                    dp[i][j + 1],
+                    dp[i + 1][j]
+                )
+            }
         }
-
-        if(nums1[i] == nums2[j]){
-           cache[Pair(i,j)]=  1  + dfs(i+1,j+1)
-        }else{
-           cache[Pair(i,j)] = maxOf(dfs(i+1,j),dfs(i,j+1))
-
-        }
-        return cache[Pair(i,j)]!!
-
     }
-    return dfs(0,0)
+    return dp[nums2.size][nums1.size]
+
 }
